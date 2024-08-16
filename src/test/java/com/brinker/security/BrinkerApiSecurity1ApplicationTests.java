2c6925603c649c947994d1d239b610540c77fd85
nageshgitversion <rasukuntanagesh@gmail.com>
2024-08-16 13:40:51 +0530

    first commit

---
+++ src/test/java/com/brinker/security/BrinkerApiSecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brinker.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrinkerApiSecurity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
